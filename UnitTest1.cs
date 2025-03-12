3a3be2efe8b74a2ea299f32e48c278654aaee140
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2025-03-12 14:00:00 +0100

    CalcularEnergiaFuncionaCorrectament Closes #1

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -6,14 +6,34 @@ namespace EcoEnergyRazorTesting
         [Fact]
         public void Test1()
         {
-            SistemaEnergia simulacio = new SistemaEnergia(DateTime.Now, "Solar", 0, 2, 0.2, 0.5);
+            SistemaEnergia simulacio = new SistemaEnergia(DateTime.Now, "Solar", 0, 3, 0.2, 0.5);
             Assert.Equal(simulacio.Energia, 0);
         }
         [Fact]
         public void Test2()
         {
-            SistemaEnergia simulacio = new SistemaEnergia(DateTime.Now, "Solar", 20, 2, 0.2, 0.5);
-            Assert.Equal(simulacio.Energia, 40);
+            SistemaEnergia simulacio = new SistemaEnergia(DateTime.Now, "Solar", 20, 3, 0.2, 0.5);
+            Assert.Equal(simulacio.Energia, 60);
+        }
+        [Fact]
+        public void Test3()
+        {
+            SistemaEnergia simulacio = new SistemaEnergia(DateTime.Now, "Solar", 5, 0.1, 0.2, 0.5);
+            Assert.Equal(simulacio.Energia, 0.5);
+        }
+        [Fact]
+        public void Test4()
+        {
+            SistemaEnergia simulacio = new SistemaEnergia(DateTime.Now, "Solar", 20, 0.1, 0.2, 0.5);
+            Assert.Equal(simulacio.Energia, 2);
+        }
+    }
+    public class CalcularCostTotalTesting
+    {
+        [Fact]
+        public void Test1()
+        {
+
         }
     }
 }
